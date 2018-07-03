a6a52f39110a200a5651a9146911509f3f1e7074
syashavanth <syashavanth@umass.edu>
2018-07-02 23:18:44 -0400

    Starting a new repo with init files

---
+++ scanner.cpp
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#include<string>
+#include<iostream>
+#include"scanner.h"
+using namespace std;
+
+void Tokenise(string filename)
+{
+    
+}
---
+++ scanner.h
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   scanner.h
+ * Author: vardh
+ *
+ * Created on July 2, 2018, 10:57 PM
+ */
+
+#ifndef SCANNER_H
+#define SCANNER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum TOKENS {IDENTIFIER=1, KEYWORDS, SPL_CHAR, OPERATOR};
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SCANNER_H */
+
