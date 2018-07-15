6a1744185b038fca5db1fd9c11660309ff729c19
syashavanth <syashavanth@cs.umass.edu>
2018-07-15 12:46:34 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,25 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.cpp
- * Author: vardh
- *
- * Created on July 2, 2018, 9:47 PM
- */
-
 #include <cstdlib>
-#include "scanner.h"
+#include "lexer.h"
+#include <iostream>
 using namespace std;
 
 
 int main(int argc, char** argv) {
 
-    token t;
-    t.Tokenise("test_input.txt");
-    return 0;
-}
+	//cout << "HELLO";
+	Tokenise("test_input.txt");
 
+	getchar();
+	return 0;
+}
