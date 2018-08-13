16b242a58ec26b4437c7fd95ea6ba5fac3379a11
syashavanth <syashavanth@cs.umass.edu>
2018-08-14 01:25:24 +0530

    skeleton created.

--- Parser.h
+++ Parser.h
@@ -11,5 +11,56 @@ public:
 	bool program();
 	bool data_decl();
 	bool func_list();
+	bool func();
+	bool func1();
+	bool func_decl();
+	
+	bool type_name();
+	bool parameter_list();
+	bool nonempty_list();
+	bool extended_nonempty_list();
+	
+	bool id_list();
+	bool extended_id_list();
+	bool id();
+	bool id1();
+
+	bool block_statements();
+	bool statements();
+	bool statement();
+
+	bool assignment();
+	bool func_call();
+	bool expr_list();
+	bool nonempty_expr_list();
+	bool ext_nonempty_expr_list();
+
+	bool if_statement();
+
+	bool condition_expr();
+	bool condition_expr1();
+	bool condition_op();
+	bool Logical_AND();
+
+	bool condition();
+	bool comparison_op();
+	bool while_statement();
+	bool return_statement();
+	bool return1();
+
+	bool break_statement();
+	bool continue_statement();
+	bool expression();
+	bool extended_expression();
+
+	bool addop();
+	bool term();
+	bool extended_term();
+	
+	bool mulop();
+
+	bool factor();
+	bool factor1();
+
 
 };
