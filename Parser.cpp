1b91dd8fae79a1dc13de5c1dd012b3894f4dc933
syashavanth <syashavanth@cs.umass.edu>
2018-08-14 01:25:47 +0530

    Update Parser.cpp

--- Parser.cpp
+++ Parser.cpp
@@ -36,3 +36,199 @@ bool Parser::func_list()
 {
 	return true;
 }
+
+bool Parser::func()
+{
+	return false;
+}
+
+bool Parser::func1()
+{
+	return false;
+}
+
+bool Parser::func_decl()
+{
+	return false;
+}
+
+bool Parser::type_name()
+{
+	return false;
+}
+
+bool Parser::parameter_list()
+{
+	return false;
+}
+
+bool Parser::nonempty_list()
+{
+	return false;
+}
+
+bool Parser::extended_nonempty_list()
+{
+	return false;
+}
+
+bool Parser::id_list()
+{
+	return false;
+}
+
+bool Parser::extended_id_list()
+{
+	return false;
+}
+
+bool Parser::id()
+{
+	return false;
+}
+
+bool Parser::id1()
+{
+	return false;
+}
+
+bool Parser::block_statements()
+{
+	return false;
+}
+
+bool Parser::statements()
+{
+	return false;
+}
+
+bool Parser::statement()
+{
+	return false;
+}
+
+bool Parser::assignment()
+{
+	return false;
+}
+
+bool Parser::func_call()
+{
+	return false;
+}
+
+bool Parser::expr_list()
+{
+	return false;
+}
+
+bool Parser::nonempty_expr_list()
+{
+	return false;
+}
+
+bool Parser::ext_nonempty_expr_list()
+{
+	return false;
+}
+
+bool Parser::if_statement()
+{
+	return false;
+}
+
+bool Parser::condition_expr()
+{
+	return false;
+}
+
+bool Parser::condition_expr1()
+{
+	return false;
+}
+
+bool Parser::condition_op()
+{
+	return false;
+}
+
+bool Parser::Logical_AND()
+{
+	return false;
+}
+
+bool Parser::condition()
+{
+	return false;
+}
+
+bool Parser::comparison_op()
+{
+	return false;
+}
+
+bool Parser::while_statement()
+{
+	return false;
+}
+
+bool Parser::return_statement()
+{
+	return false;
+}
+
+bool Parser::return1()
+{
+	return false;
+}
+
+bool Parser::break_statement()
+{
+	return false;
+}
+
+bool Parser::continue_statement()
+{
+	return false;
+}
+
+bool Parser::expression()
+{
+	return false;
+}
+
+bool Parser::extended_expression()
+{
+	return false;
+}
+
+bool Parser::addop()
+{
+	return false;
+}
+
+bool Parser::term()
+{
+	return false;
+}
+
+bool Parser::extended_term()
+{
+	return false;
+}
+
+bool Parser::mulop()
+{
+	return false;
+}
+
+bool Parser::factor()
+{
+	return false;
+}
+
+bool Parser::factor1()
+{
+	return false;
+}
+
