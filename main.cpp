f0cb97625e50a35d519adf64b70c630cc6f534a2
syashavanth <syashavanth@cs.umass.edu>
2018-08-14 00:02:23 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include <cstdlib>
 #include "lexer.h"
 #include <iostream>
+#include "Parser.h"
 using namespace std;
 
 
 int main(int argc, char** argv) {
 
 	//cout << "HELLO";
-	Tokenise("test_input.txt");
+	Lexer L;
+	L.Tokenise("test_input.txt");
+	vector<token*> list_of_toks = L.get_All_Tokens();
+	Parser P(list_of_toks);
 
 	getchar();
 	return 0;
